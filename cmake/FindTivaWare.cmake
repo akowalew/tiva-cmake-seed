d49a8cf34ba19070db8d6bada0495f208aa5ac61
akowalew <ram.techen@gmail.com>
2017-12-06 14:11:56 +0100

    Improved code style in FindTivaWare.cmake

--- cmake/FindTivaWare.cmake
+++ cmake/FindTivaWare.cmake
@@ -5,13 +5,17 @@
 # 	TIVAWARE_INCLUDE_DIRS - The TivaWare include directories
 # Author: akowalew
 
-find_path(_TIVAWARE_INCLUDE_DIR inc PATH_SUFFIXES tivaware)
+include(FindPackageHandleStandardArgs)
+
+find_path(_TIVAWARE_INCLUDE_DIR inc 
+	PATH_SUFFIXES tivaware)
 
 if("driverlib" IN_LIST TivaWare_FIND_COMPONENTS)
-	find_path(_TIVAWARE_DRIVERLIB_INCLUDE_DIR driverlib PATH_SUFFIXES tivaware)
-	find_library(_TIVAWARE_DRIVERLIB_LIBRARY driver PATH_SUFFIXES tivaware)
+	find_path(_TIVAWARE_DRIVERLIB_INCLUDE_DIR driverlib 
+		PATH_SUFFIXES tivaware)
+	find_library(_TIVAWARE_DRIVERLIB_LIBRARY driver 
+		PATH_SUFFIXES tivaware)
 
-	include(FindPackageHandleStandardArgs)
 	find_package_handle_standard_args(TivaWare
 		DEFAULT_MSG
 		_TIVAWARE_INCLUDE_DIR
@@ -20,9 +24,12 @@ if("driverlib" IN_LIST TivaWare_FIND_COMPONENTS)
 	)
 
 	set(TivaWare_INCLUDE_DIRS 
-		${_TIVAWARE_INCLUDE_DIR})
+		${_TIVAWARE_INCLUDE_DIR}
+	)
 
-	set(TivaWare_LIBRARIES ${_TIVAWARE_DRIVERLIB_LIBRARY})
+	set(TivaWare_LIBRARIES 
+		${_TIVAWARE_DRIVERLIB_LIBRARY}
+	)
 
 	mark_as_advanced(
 		_TIVAWARE_INCLUDE_DIR
@@ -31,13 +38,14 @@ if("driverlib" IN_LIST TivaWare_FIND_COMPONENTS)
 	)
 
 else()
-	include(FindPackageHandleStandardArgs)
 	find_package_handle_standard_args(TivaWare
 		DEFAULT_MSG
 		_TIVAWARE_INCLUDE_DIR
 	)
 
-	set(TivaWare_INCLUDE_DIRS ${_TIVAWARE_INCLUDE_DIR})
+	set(TivaWare_INCLUDE_DIRS 
+		${_TIVAWARE_INCLUDE_DIR}
+	)
 
 	mark_as_advanced(
 		_TIVAWARE_INCLUDE_DIR
